eaa4ee3216e260d3ffdebf79682b227ac9f4b1de
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2025-10-06 18:58:10 -0500

    Update test_smoke.py

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,5 +1,5 @@
 def test_settings_loaded(django_settings):
-    assert django_settings.SECRET_KEY, "SECRET_KEY debe estar definido"
+    assert django_settings.SECRET_KEY, "dev-secret-papers"
 
 def test_math():
     assert 2 + 2 == 4
